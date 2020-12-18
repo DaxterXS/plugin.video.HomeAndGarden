39e5e45daa30870b8ba81bbca7c1133b94f73047
Davide Bisso <33525392+DaxterXS@users.noreply.github.com>
2020-12-18 22:51:14 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -9,7 +9,7 @@ try:
 	streaming_url = 'https://sbshdlu5-lh.akamaihd.net/i/sbshdl_7@106896/master.m3u8?hdnts=st=1584575040~exp=1584661440~acl=/i/*~hmac=408719d9932fa30c431509eb224d265c6fca2b4a1c5ef014c4ac8e67a183f024&mux_audio=true'
 	thumb_url = 'https://' + re.findall('"img":"https{0,1}://(.*?)"', res.text)[1]
 	listitem = xbmcgui.ListItem('Cielo TV')
-	listitem.setInfo('video', {'Title': 'HomeAndGarden TV'})
+	listitem.setInfo('video', {'Title': 'Cielo TV'})
 	listitem.setArt({ 'thumb': thumb_url})
 	xbmc.Player().play(streaming_url, listitem)
 
