f5a9a9d965b97da201fd1a67e3db359ab3600d40
Davide Bisso <33525392+DaxterXS@users.noreply.github.com>
2020-12-18 22:44:17 +0100

    Update addon.py

--- addon.py
+++ addon.py
@@ -8,13 +8,13 @@ res = requests.get(url)
 try:
 	streaming_url = 'https://sbshdlu5-lh.akamaihd.net/i/sbshdl_7@106896/master.m3u8?hdnts=st=1584575040~exp=1584661440~acl=/i/*~hmac=408719d9932fa30c431509eb224d265c6fca2b4a1c5ef014c4ac8e67a183f024&mux_audio=true'
 	thumb_url = 'https://' + re.findall('"img":"https{0,1}://(.*?)"', res.text)[1]
-	listitem = xbmcgui.ListItem('Home and Garden TV')
-	listitem.setInfo('video', {'Title': 'Home and Garden  TV'})
+	listitem = xbmcgui.ListItem('HomeandGarden TV')
+	listitem.setInfo('video', {'Title': 'HomeandGarden  TV'})
 	listitem.setArt({ 'thumb': thumb_url})
 	xbmc.Player().play(streaming_url, listitem)
 
 except Exception as ex:
 	message = 'Eccezione: %s' % ex
-	listitem = xbmcgui.ListItem('Home and Garden  - Errore')
-	listitem.setInfo('video', {'Title': 'Errore nell\' avvio del plugin Home and Garden '})
+	listitem = xbmcgui.ListItem('HomeandGarden  - Errore')
+	listitem.setInfo('video', {'Title': 'Errore nell\' avvio del plugin HomeandGarden '})
 	listitem.setInfo('video', { 'plot': message })
